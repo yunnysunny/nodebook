cdc90a01412b2a01856ca52a068d3ec244f70877
yunnysunny <yunnysunny@gmail.com>
2022-01-19 20:16:10 +0800

    增加调试日志

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -34,8 +34,8 @@ docker build --target build-stage \
     --cache-from type=local,src=${CACHE_FILE} \
     --cache-to type=local,mode=max,dest=${CACHE_FILE} \
     --progress=plain .
-docker images
 docker build --output output .
+info_print "rename to output/${GITHUB_SHA}.pdf"
 mv output/book.pdf "output/${GITHUB_SHA}.pdf"
 
 #pandoc -N -s --toc  -f markdown+smart  --pdf-engine=xelatex -V CJKmainfont='KaiTi'  -V geometry:margin=1in 00_preface.md 01_node_introduce.md 02_node_javascript.md 03_node_basic.md 04_node_npm.md 05_node_database.md 06_node_express_basic.md 07_node_express_advance.md 08_node_unit_test.md 09_node_production.md 10_node_addon.md 11_node_optimization.md 12_node_web_security.md a1_node_utils.md a2_bibliography.md a3_convention.md a4_node_http.md a5_node_multi_versions.md a6_node_native_addon_config.md -o output.pdf
