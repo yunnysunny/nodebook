8136a8059d40ff9b51446ddbc689008c62c63c76
yunnysunny <yunnysunny@gmail.com>
2022-01-19 20:03:59 +0800

    重命名生成的pdf文件

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -36,6 +36,6 @@ docker build --target build-stage \
     --progress=plain .
 docker images
 docker build --output output .
-# save_cache ${SAVE_NAME} ${BUILD_TAG}
+mv output/book.pdf "output/${GITHUB_SHA}.pdf"
 
 #pandoc -N -s --toc  -f markdown+smart  --pdf-engine=xelatex -V CJKmainfont='KaiTi'  -V geometry:margin=1in 00_preface.md 01_node_introduce.md 02_node_javascript.md 03_node_basic.md 04_node_npm.md 05_node_database.md 06_node_express_basic.md 07_node_express_advance.md 08_node_unit_test.md 09_node_production.md 10_node_addon.md 11_node_optimization.md 12_node_web_security.md a1_node_utils.md a2_bibliography.md a3_convention.md a4_node_http.md a5_node_multi_versions.md a6_node_native_addon_config.md -o output.pdf
