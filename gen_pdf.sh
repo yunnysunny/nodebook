4e641532fa2eb59d66c5999d04c05c84f0cf5538
yunnysunny <yunnysunny@gmail.com>
2022-01-19 17:15:31 +0800

    修正docker缓存写入错误

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -40,18 +40,18 @@ save_cache() {
     FROM_PATH="${CACHE_FROM_DIR}/${SAVE_NAME}.tar"
     if [ -d "$CACHE_FROM_DIR" ] ; then
         echo save "$@" to "$FROM_PATH"
-        docker save "$@" > "$FROM_PATH"
+        docker save "$(docker images -q)" > "$FROM_PATH"
     else
         echo cache directory "$CACHE_FROM_DIR" not exist
     fi
 }
-TAG_NAME=nodebook
 SAVE_NAME=all_images
 mkdir -p output
 
 load_cache ${SAVE_NAME}
 docker pull ubuntu:20.04
-DOCKER_BUILDKIT=1 docker build --file Dockerfile --output output --progress=plain -t ${TAG_NAME} .
-save_cache ${SAVE_NAME} ubuntu:20.04  ${TAG_NAME}
+DOCKER_BUILDKIT=1 docker build --file Dockerfile --output output  --progress=plain .
+docker images
+save_cache ${SAVE_NAME}
 
 #pandoc -N -s --toc  -f markdown+smart  --pdf-engine=xelatex -V CJKmainfont='KaiTi'  -V geometry:margin=1in 00_preface.md 01_node_introduce.md 02_node_javascript.md 03_node_basic.md 04_node_npm.md 05_node_database.md 06_node_express_basic.md 07_node_express_advance.md 08_node_unit_test.md 09_node_production.md 10_node_addon.md 11_node_optimization.md 12_node_web_security.md a1_node_utils.md a2_bibliography.md a3_convention.md a4_node_http.md a5_node_multi_versions.md a6_node_native_addon_config.md -o output.pdf
